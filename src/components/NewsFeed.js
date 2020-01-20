dffc7560cd703d6304f75195f1e2ec7cd73c572a
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-01-20 17:15:01 +0100

    added props and defaults to NewsFeed.js

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Component } from "react";
+import PropTypes from "prop-types";
 
 class NewsFeed extends Component {
   render() {
     return (
       <div>
-        <h2>TITLE</h2>
-        <p>this will contain the description of the headline</p>
+        <h2>{this.props.heading}</h2>
+        <p>{this.props.description}</p>
       </div>
     );
   }
 }
+
+NewsFeed.propTypes = {
+  heading: PropTypes.string,
+  description: PropTypes.string
+};
+
+NewsFeed.defaultProps = {
+  heading: "This needs to be the headline",
+  description: "Description goes here"
+};
 
 export default NewsFeed;
