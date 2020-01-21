23bdda46096aa7833b1685832be37cd533dcbb50
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-01-21 16:07:03 +0100

    Added api fetch

--- src/App.js
+++ src/App.js
@@ -9,19 +9,14 @@ function App() {
       <header className="App-header">
         <h1 className="newsfeedHeader">DILIP BRIAN NEWSFEED APP</h1>
       </header>
-      <div class="left">
+      <div className="left">
         <NewsFeed
           heading={"Bananas are awesome!"}
           description={"They are usually very yellow"}
         />
         <NewsFeed />
-        <NewsFeed />
       </div>
-      <div className="right">
-        <NewsFeed />
-        <NewsFeed />
-        <NewsFeed />
-      </div>
+      <div className="right"></div>
       <UserSubmit />
       <footer>This will be the footer</footer>
     </div>
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -3,11 +3,34 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 
 class NewsFeed extends Component {
+  state = {
+    title: "",
+    description: ""
+  };
+
+  componentDidMount() {
+    fetch(
+      "https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-21&sortBy=publishedAt&apiKey=41f62212190b4ef68512cf121cfc796b"
+    )
+      .then(response => response.json())
+      .then(news => {
+        console.log(news);
+        this.setState({
+          title: news.articles[0].title,
+          description: news.articles[0].description
+        });
+      })
+      .catch(err => {
+        console.warn("error", err);
+      });
+  }
+
   render() {
+    //console.log("checkcing this.state", this.state);
     return (
       <div className="newsitem">
-        <h2>{this.props.heading}</h2>
-        <p>{this.props.description}</p>
+        <h2>{this.state.title}</h2>
+        <p>{this.state.description}</p>
       </div>
     );
   }
